ac12e9b8fb40dc4404b66c41d8259d670ebb464f
Cameron Bell <cameron.bellboy@gmail.com>
2018-05-09 20:34:36 +1200

    Extremely Basic Options Menu added

---
+++ src/Game/Screens/OptionsScreen.java
@@ -0,0 +1,95 @@
+package Game.Screens;
+
+import Game.Data.Settings;
+import Game.Display.DisplayElements.*;
+import Game.Display.UserInterface.TextManager;
+import Game.Handler;
+
+import java.awt.*;
+
+public class OptionsScreen extends Screen {
+// VARIABLES //
+    // Statics //
+    private static final int SPACE_BETWEEN_COLUMNS = 24;
+    private static final int SPACE_BETWEEN_ROWS = 16;
+    private static final int BORDER_WIDTH = 1;
+    private static final int OUTER_PADDING = 24;
+
+    private Screen returnScreen;
+
+    Color backgroundColor;
+    Color borderColor;
+    Color fillColour;
+
+    // Elements //
+    PaddedElement mainElement;
+    VerticalListElement titleWrapper;
+    VerticalListElement mainColumn;
+    ButtonElement backButton;
+    ButtonElement clearScoresButton;
+
+
+// CONSTRUCTORS //
+    public OptionsScreen(Screen returnScreen) {
+        this.returnScreen = returnScreen;
+
+        borderColor = new Color(129,130,174);
+        fillColour = new Color(78, 78, 122, 58);
+        backgroundColor = new Color(0, 0, 20);
+
+        fillElements();
+    }
+
+    // METHODS //
+
+    @Override
+    public void update() {
+        backButton.update();
+        clearScoresButton.update();
+    }
+    @Override
+    public void draw(Graphics g) {
+        // Draw Background
+        g.setColor(backgroundColor);
+        g.fillRect(0, 0, Settings.game_width, Settings.game_height);
+
+        // Draw Menu
+        int xStart = (Settings.game_width / 2) - (mainElement.getWidth() / 2);
+        int yStart = (Settings.game_height / 2) - (mainElement.getHeight() / 2);
+
+        mainElement.draw(g, xStart, yStart);
+
+        // Draw Back Button
+        backButton.draw(g, xStart - backButton.getWidth() + 1, yStart);
+    }
+
+    private void fillElements() {
+        mainElement = new PaddedElement(1, borderColor, fillColour, OUTER_PADDING);
+        titleWrapper = new VerticalListElement((int)(SPACE_BETWEEN_ROWS*1.5));
+        mainColumn = new VerticalListElement(SPACE_BETWEEN_ROWS);
+
+        // Add Elements
+        clearScoresButton = new ButtonElement("CLEAR HIGH SCORES", BORDER_WIDTH, borderColor, fillColour, Settings.button_padding) {
+            @Override
+            protected void onClick() {
+                Handler.get().getSave().clearScores();
+
+            }
+        };
+        mainColumn.addChild(clearScoresButton);
+
+        titleWrapper.addChild(new TextElement("OPTIONS"));
+        titleWrapper.addChild(mainColumn);
+        titleWrapper.setCenterAlign(true);
+
+        mainElement.setChildElement(titleWrapper);
+
+        // Back Button
+        backButton = new ButtonElement("<", BORDER_WIDTH, borderColor, fillColour, Settings.button_padding) {
+            @Override
+            protected void onClick() {
+                ScreenManager.setScreen(returnScreen);
+            }
+        };
+    }
+}
