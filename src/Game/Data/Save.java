5985206da1804150954d0f75b6b91216b1798d12
Cameron Bell <cameron.bellboy@gmail.com>
2018-06-13 17:56:17 +1200

    Changed ScoreBoard XML save file name & axxed xml tag to beginning of document

--- src/Game/Data/Save.java
+++ src/Game/Data/Save.java
@@ -9,7 +9,8 @@ import java.io.*;
  */
 public class Save {
 // VARIABLES //
-    private static final String DEF_XML_SAVE_PATH = "data/save.xml";
+    private static final String XML_TAG = "<?xml version=\"1.0\"?>";
+    private static final String DEF_XML_SCOREBOARD_SAVE_PATH = "data/scores.xml";
 
     // Data //
     private ScoreBoard scoreBoard;
@@ -22,24 +23,24 @@ public class Save {
 // METHODS //
     // Method - Save Data to File // TODO // Implement Saved Settings
     public void saveXML() {
-        File file = new File(DEF_XML_SAVE_PATH);
+        File file = new File(DEF_XML_SCOREBOARD_SAVE_PATH);
         file.getParentFile().mkdirs();
         try {
             BufferedWriter br = new BufferedWriter(new FileWriter(file));
-            br.write(scoreBoard.toXML());
+            br.write(XML_TAG + scoreBoard.toXML());
             br.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        scoreBoard.fromXML(DEF_XML_SAVE_PATH); // TODO // REMOVE
+        scoreBoard.fromXML(DEF_XML_SCOREBOARD_SAVE_PATH); // TODO // REMOVE
     }
 
     // Method - Load Save Data from File // TODO // Implement Saved Settings
     public boolean loadXML() {
-        if(!(new File(DEF_XML_SAVE_PATH).isFile())) saveXML();
+        if(!(new File(DEF_XML_SCOREBOARD_SAVE_PATH).isFile())) saveXML();
 
-        return scoreBoard.fromXML(DEF_XML_SAVE_PATH);
+        return scoreBoard.fromXML(DEF_XML_SCOREBOARD_SAVE_PATH);
     }
 
     // Method - Turn Save File into a String //
